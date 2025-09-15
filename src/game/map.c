f2288ccadd4c19691eabf4e10c835c00958acf0f
minhquan <s3938194@rmit.edu.vn>
2025-09-16 04:20:25 +0700

    Update all requirement files to render for map 1
    initiate map 1, which contains all components

--- src/game/map.c
+++ src/game/map.c
@@ -1,5 +1,20 @@
 #include "../../includes/framebf.h"
 #include "map.h"
+#include "../../assets/images/wall1.c"
+#include "../../assets/images/wall2.c"
+#include "../../assets/images/wall3.c"
+#include "../../assets/images/road.c"
+#include "../../assets/images/baba.c"
+#include "../../assets/images/is.c"
+#include "../../assets/images/you.c"
+#include "../../assets/images/flag_txt.c"
+#include "../../assets/images/win.c"
+#include "../../assets/images/wall_txt.c"
+#include "../../assets/images/stop.c"
+#include "../../assets/images/rock_txt.c"
+#include "../../assets/images/push.c"
+#include "../../assets/images/rock.c"
+#include "../../assets/images/flag.c"
 
 // Define size_t manually for bare-metal
 typedef unsigned long size_t;
@@ -24,33 +39,33 @@ void *memset(void *s, int c, unsigned long n) {
 char map[24][32]; // accessible from player.c
 
 void LoadLevel1(void) {
-    char tmp[24][32] = {
-        "                                ",
-        "   WWWWWWWWWWWWWWWWWWWWWWWWWW   ",
-        "   W        P                 W",
-        "   W   WWW      WWWW   WWW    W",
-        "   W   W   W              W   W",
-        "   W   W   WWWW   WWWW    W   W",
-        "   W   W                  W   W",
-        "   W   WWWW   WW   WWWW   W   W",
-        "   W                        F W",
-        "   W   W   WWWW   WWWW    W   W",
-        "   W   W                  W   W",
-        "   W   W   W              W   W",
-        "   W   WWW      WWWW   WWW    W",
-        "   W                        W W",
-        "   WWWWWWWWWWWWWWWWWWWWWWWWWWWW",
+    char tmp[MAP_HEIGHT][MAP_WIDTH] = {
         "                                ",
         "                                ",
+        "  a b c                  d b e  ",
         "                                ",
-        "                                ", 
-        "                                ", 
+        " QWWWWWWWWWWWWWWWWWWWWWWWWWWWWE ",
+        "               R                ",
+        "               R                ",
+        "               R                ",
+        "               R       Z        ",
+        "               R                ",
+        "               R                ",
+        "               R                ",
+        " QWWWWWWWWWWWWWWWWWWWWWWWWWWWWE ",
+        "                                ",
+        "  g b f                  h b i  ",
         "                                ",
         "                                ",
         "                                ",
         "                                ",
+        "                                ",
+        "                                ",
+        "                                ",
+        "                                ",
+        "                                "
     };
-    map_memcpy(map, tmp, sizeof(tmp));
+    map_memcpy(map, tmp, sizeof(map));
 }
 
 void LoadLevel2(void) {
@@ -83,26 +98,122 @@ void LoadLevel2(void) {
     map_memcpy(map, tmp, sizeof(tmp));
 }
 
+void drawSpriteARGB32Scaled(const unsigned long *sprite, int srcW, int srcH,
+                            int dstW, int dstH, int dstX, int dstY) {
+    for (int y = 0; y < dstH; y++) {
+        int sy = (y * srcH) / dstH;
+        for (int x = 0; x < dstW; x++) {
+            int sx = (x * srcW) / dstW;
+            unsigned long color = sprite[sy * srcW + sx];
+
+            unsigned char r = (color >> 24) & 0xFF;
+            unsigned char g = (color >> 16) & 0xFF;
+            unsigned char b = (color >>  8) & 0xFF;
+            unsigned char a =  color        & 0xFF;
+
+            if (a == 0) continue; // skip fully transparent
+
+            // Skip "outline/background" pixels that are blue-dominant
+            if (b > r + 20 && b > g + 20) {
+                continue;
+            }
+            // Otherwise draw normally
+            drawPixelARGB32(dstX + x, dstY + y, color);
+        }
+    }
+}
+
 void drawMap(void) {
-    for (int i = 0; i < 24; i++) {
-        for (int j = 0; j < 32; j++) {
+    for (int i = 0; i < MAP_HEIGHT; i++) {
+        for (int j = 0; j < MAP_WIDTH; j++) {
             int x = j * TILE_SIZE;
             int y = i * TILE_SIZE;
+            char c = map[i][j];
 
-            if (map[i][j] == 'W') {
-                drawRectARGB32(x, y, x + TILE_SIZE, y + TILE_SIZE, 0x000000CC, 2); // solid wall
-            } else if (map[i][j] == 'P') {
-                drawRectARGB32(x, y, x + TILE_SIZE, y + TILE_SIZE, 0x0000CC99, 2); // passable wall (different shade)
-            } else {
-                drawRectARGB32(x, y, x + TILE_SIZE, y + TILE_SIZE, 0x00000000, 1); // background/floor
+            switch (c) {
+                case 'Q': // left wall
+                    drawSpriteARGB32Scaled(epd_bitmap_tile003, TILE_SIZE, TILE_SIZE,
+                                           TILE_SIZE, TILE_SIZE, x, y);
+                    break;
+
+                case 'W': // middle wall
+                    drawSpriteARGB32Scaled(epd_bitmap_tile007, TILE_SIZE, TILE_SIZE,
+                                           TILE_SIZE, TILE_SIZE, x, y);
+                    break;
+
+                case 'E': // right wall
+                    drawSpriteARGB32Scaled(epd_bitmap_tile006, TILE_SIZE, TILE_SIZE,
+                                           TILE_SIZE, TILE_SIZE, x, y);
+                    break;
+
+                case 'F': //floor or tiles
+                    drawSpriteARGB32Scaled(road_tile, TILE_SIZE, TILE_SIZE,
+                                           TILE_SIZE, TILE_SIZE, x, y);
+                    break;
+
+                case 'R': //rock
+                    drawSpriteARGB32Scaled(epd_bitmap_rock, TILE_SIZE, TILE_SIZE,
+                                           TILE_SIZE, TILE_SIZE, x, y);
+                    break;
+
+                case 'Z': //flag
+                    drawSpriteARGB32Scaled(epd_bitmap_flag, TILE_SIZE, TILE_SIZE,
+                                           TILE_SIZE, TILE_SIZE, x, y);
+                    break;
+
+                case 'a': //baba text
+                    drawSpriteARGB32Scaled(epd_bitmap_baba, TILE_SIZE, TILE_SIZE,
+                                           TILE_SIZE, TILE_SIZE, x, y);
+                    break;
+
+                case 'b': //is text
+                    drawSpriteARGB32Scaled(epd_bitmap_is, TILE_SIZE, TILE_SIZE,
+                                           TILE_SIZE, TILE_SIZE, x, y);
+                    break;
+
+                case 'c': //you text
+                    drawSpriteARGB32Scaled(epd_bitmap_you, TILE_SIZE, TILE_SIZE,
+                                           TILE_SIZE, TILE_SIZE, x, y);
+                    break;
+
+                case 'd': //flag text
+                    drawSpriteARGB32Scaled(epd_bitmap_flag_text, TILE_SIZE, TILE_SIZE,
+                                           TILE_SIZE, TILE_SIZE, x, y);
+                    break;
+
+                case 'e': //win text
+                    drawSpriteARGB32Scaled(epd_bitmap_win, TILE_SIZE, TILE_SIZE,
+                                           TILE_SIZE, TILE_SIZE, x, y);
+                    break;
+
+                case 'f': //stop text
+                    drawSpriteARGB32Scaled(epd_bitmap_stop, TILE_SIZE, TILE_SIZE,
+                                           TILE_SIZE, TILE_SIZE, x, y);
+                    break;
+
+                case 'g': //wall text
+                    drawSpriteARGB32Scaled(epd_bitmap_wall, TILE_SIZE, TILE_SIZE,
+                                           TILE_SIZE, TILE_SIZE, x, y);
+                    break;
+
+                case 'h': //rock text
+                    drawSpriteARGB32Scaled(epd_bitmap_rock_text, TILE_SIZE, TILE_SIZE,
+                                           TILE_SIZE, TILE_SIZE, x, y);
+                    break;
+                
+                case 'i': //push text
+                    drawSpriteARGB32Scaled(epd_bitmap_push, TILE_SIZE, TILE_SIZE,
+                                           TILE_SIZE, TILE_SIZE, x, y);
+                    break;
+
+                default:
+                    // empty, skip
+                    break;
             }
         }
     }
 
-    for (int x = 0; x < 800; x++) {
-        drawPixelARGB32(x, 550, 0xFFFFFFFF);  // 1px horizontal white line
-    }
-
-    drawString(20, 560, "     WASD: Move  |  B: Back  |  Q: Main Menu", 0xFFFFFF, 2);
+    // UI
+    for (int x = 0; x < 800; x++) drawPixelARGB32(x, 550, 0xFFFFFFFF);
+    drawString(20, 560, " WASD: Move | B: Back | Q: Main Menu", 0xFFFFFF, 2);
 }
-
